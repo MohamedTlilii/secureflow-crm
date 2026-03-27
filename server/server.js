26a9ada300dea340970492da6f7335ca9c5a90f2
Mohamed Tlili <Mohamedtlili1995@hotmail.com>
2026-03-27 07:48:52 -0400

    Fix CORS: Ajout de l'URL exacte Vercel

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
+    'https://secureflow-crm.vercel.app',
     process.env.CLIENT_URL
   ].filter(Boolean),
   credentials: true
